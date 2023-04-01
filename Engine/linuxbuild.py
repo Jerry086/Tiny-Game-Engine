84d2e8ddc6c6bc54980914fa9891839e661e358f
K <kzk5566@protonmail.com>
2023-04-01 14:40:01 -0700

    Fix linux build script

--- Engine/linuxbuild.py
+++ Engine/linuxbuild.py
@@ -10,25 +10,39 @@
 
 import os
 
-COMPILER="g++"
+COMPILER = "g++"
 
-SOURCE="./src/*.cpp"
+# SOURCE="./src/*.cpp"
+SOURCE = "./src/*.cpp ./src/Components/*.cpp"
 
 # You can can add other arguments as you see fit.
 # What does the "-D LINUX" command do?
-ARGUMENTS="-D LINUX -std=c++14 -shared -fPIC"
+ARGUMENTS = "-D LINUX -std=c++14 -shared -fPIC"
 
 # Which directories do we want to include.
-INCLUDE_DIR="-I ./include/ -I./pybind11/include/ `python3.8 -m pybind11 --includes`"
+INCLUDE_DIR = "-I ./include/ -I./include/Components -I./pybind11/include/ `python3.8 -m pybind11 --includes`"
 
 # What libraries do we want to include
-LIBRARIES="-lSDL2 -ldl `python3.8-config --ldflags`"
+LIBRARIES = "-lSDL2 -ldl `python3.8-config --ldflags`"
 
 # The name of our module
-EXECUTABLE="mygameengine.so"
+EXECUTABLE = "mygameengine.so"
 
 # Build a string of our compile commands that we run in the terminal
-compileString=COMPILER+" "+ARGUMENTS+" -o "+EXECUTABLE+" "+" "+INCLUDE_DIR+" "+SOURCE+" "+LIBRARIES
+compileString = (
+    COMPILER
+    + " "
+    + ARGUMENTS
+    + " -o "
+    + EXECUTABLE
+    + " "
+    + " "
+    + INCLUDE_DIR
+    + " "
+    + SOURCE
+    + " "
+    + LIBRARIES
+)
 
 # Print out the compile string
 print(compileString)
