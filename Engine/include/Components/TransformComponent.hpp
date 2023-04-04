779ad5ebcd96f3daaf818b69e98c52af7176b96a
Szeka <tse.s@northeastern.edu>
2023-04-04 11:35:09 -0700

    fix compilation error on linux

--- Engine/include/Components/TransformComponent.hpp
+++ Engine/include/Components/TransformComponent.hpp
@@ -1,24 +1,26 @@
 #ifndef TRANSFORMCOMPONENT_HPP
 #define TRANSFORMCOMPONENT_HPP
 
-#include "Vec.hpp"
+#include <memory>
+
 #include "Component.hpp"
 #include "ControllerComponent.hpp"
+#include "Vec.hpp"
 
-class TransformComponent : public Component
-{
-public:
+class TransformComponent : public Component {
+   public:
     // Position of a game object
     Vec2 m_position;
     Vec2 m_direction;
     // Constructor
-    TransformComponent(Vec2 direction, Vec2 new_position, std::shared_ptr<ControllerComponent> controller);
+    TransformComponent(Vec2 direction, Vec2 new_position,
+                       std::shared_ptr<ControllerComponent> controller);
     // Destructor
     ~TransformComponent();
     // Update position of a game object
     void Update() override;
 
-private:
+   private:
     std::shared_ptr<ControllerComponent> m_controller;
 };
 
