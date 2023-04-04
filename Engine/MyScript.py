5f8f623a31ad69c51b50f5ef20fc3ffd5e89d15c
Szeka <tse.s@northeastern.edu>
2023-04-04 15:03:50 -0700

    game object definition parser

--- Engine/MyScript.py
+++ Engine/MyScript.py
@@ -1,4 +1,5 @@
 import mygameengine
+from def_parser import create_components, create_go
 import time
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
@@ -6,25 +7,7 @@ WINDOW_HEIGHT = 720
 
 SDL = mygameengine.SDLGraphicsProgram(WINDOW_WIDTH, WINDOW_HEIGHT)
 
-go = mygameengine.GameObject('GO id')
-controller_component = mygameengine.ControllerComponent()
-transform_component = mygameengine.TransformComponent(
-    mygameengine.Vec2(10, 10), mygameengine.Vec2(100, 100), controller_component)
-
-sprite_component = mygameengine.SpriteComponent(
-    './sprites/sprite.bmp', transform_component, 100, 100, 75, 87, 27, 7, 4)
-tilemap_component = mygameengine.TileMapComponent(
-    './sprites/Tiles1.bmp',8,8,64,64,20,11)
-
-# healthbar_component = mygameengine.HealthBarComponent(
-#     './sprites/healthBar.bmp', transform_component, 100, 100, 105, 1074)
-
-go.AddComponent('1', controller_component)
-go.AddComponent('2', transform_component)
-go.AddComponent('3', sprite_component)
-go.AddComponent('4', tilemap_component)
-# go.AddComponent('5', healthbar_component)
-
+go = create_go('GO id', './definitions/test_npc.json')
 
 buttons = [False, False, False, False]
 
