168a3a793df8d18aa3b0b417c09ce32c31722078
Yuhan Yue <yuhan0810@outlook.com>
2023-04-05 16:28:05 -0700

    CollisionComponent test and BehaviorComponent

--- Engine/src/Components/TransformComponent.cpp
+++ Engine/src/Components/TransformComponent.cpp
@@ -10,23 +10,41 @@ TransformComponent::TransformComponent(Vec2 new_position)
 }
 
 // Construct a transform component with an initial position (x, y)
-TransformComponent::TransformComponent(Vec2 direction, Vec2 new_position,
+TransformComponent::TransformComponent(Vec2 speed, Vec2 new_position,
                                        std::shared_ptr<ControllerComponent> controller)
     : m_controller(controller)
 {
     m_position.x = new_position.x;
     m_position.y = new_position.y;
-    m_direction.x = direction.x;
-    m_direction.y = direction.y;
+    m_speed.x = speed.x;
+    m_speed.y = speed.y;
+}
+
+// Construct a transform component with an initial position (x, y)
+TransformComponent::TransformComponent(Vec2 speed, Vec2 new_position,
+                                       std::shared_ptr<BehaviorComponent> controller)
+    : behavior_controller(controller)
+{
+    m_position.x = new_position.x;
+    m_position.y = new_position.y;
+    m_speed.x = speed.x;
+    m_speed.y = speed.y;
 }
 
 TransformComponent::~TransformComponent() {}
 
 void TransformComponent::Update()
 {
-    if (m_controller == nullptr)
+    if (m_controller == nullptr && behavior_controller != nullptr){
+        m_position.x += behavior_controller->GetDirectionX() * m_speed.x;
+        m_position.y += behavior_controller->GetDirectionY() * m_speed.y;
+    }else if(m_controller != nullptr && behavior_controller == nullptr){
+        m_position.x += m_controller->GetDirectionX() * m_speed.x;
+        m_position.y += m_controller->GetDirectionY() * m_speed.y;
+    }else{
         return;
+    }
+
     // std::cout << "x: " << m_position.x << " direction: " << m_controller->GetDirectionX() << " direction x: " << m_direction.x << std::endl;
-    m_position.x += m_controller->GetDirectionX() * m_direction.x;
-    m_position.y += m_controller->GetDirectionY() * m_direction.y;
+
 }
