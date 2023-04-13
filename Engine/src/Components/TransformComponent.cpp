cd77d3ddee521b7f5c13cdc119cc3a14c334429d
Jerry Wei <zywei86@gmail.com>
2023-04-12 21:23:43 -0700

    add comments to transformer

--- Engine/src/Components/TransformComponent.cpp
+++ Engine/src/Components/TransformComponent.cpp
@@ -1,25 +1,38 @@
 #include "./Components/TransformComponent.hpp"
 
-// Construct a transform component with an initial position (x, y)
+/**
+ * Construct a TransformComponent with an initial position
+ */
 TransformComponent::TransformComponent(Vec2 position) : m_position(position) {}
 
-// Transformer for player
+/**
+ * TransformComponent constructor for player given a speed, position and controller
+ */
 TransformComponent::TransformComponent(
     Vec2 speed, Vec2 position, std::shared_ptr<ControllerComponent> controller)
     : m_speed(speed), m_position(position), m_controller(controller) {}
-
-// Transformer for enemies
+/**
+ * TransformComponent constructor for enemy given a speed, position and behavior
+ */
 TransformComponent::TransformComponent(
     Vec2 speed, Vec2 position, std::shared_ptr<BehaviorComponent> behavior)
     : m_speed(speed), m_position(position), m_behavior(behavior) {}
-
+/**
+ * Destructor
+ */
 TransformComponent::~TransformComponent() {}
-
-void TransformComponent::Update() {
-    if (m_behavior) {
+/**
+ * Update the position of a game object if the game object is dynamic
+ */
+void TransformComponent::Update()
+{
+    if (m_behavior != nullptr)
+    {
         m_position.x += m_behavior->GetDirectionX() * m_speed.x;
         m_position.y += m_behavior->GetDirectionY() * m_speed.y;
-    } else if (m_controller) {
+    }
+    else if (m_controller != nullptr)
+    {
         m_position.x += m_controller->GetDirectionX() * m_speed.x;
         m_position.y += m_controller->GetDirectionY() * m_speed.y;
     }
