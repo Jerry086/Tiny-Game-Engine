c58a990314e42bd1aff648e152730876d0be7dd7
Szeka <tse.s@northeastern.edu>
2023-04-13 16:26:42 -0700

    counter comments

--- Engine/src/Components/CounterComponent.cpp
+++ Engine/src/Components/CounterComponent.cpp
@@ -1,19 +1,48 @@
 #include "./Components/CounterComponent.hpp"
 
+/**
+ * Constructor
+ */
 CounterComponent::CounterComponent() {}
 
+/**
+ * Destructor
+ */
 CounterComponent::~CounterComponent() {}
 
+/**
+ * Update the component. Does nothing.
+ */
 void CounterComponent::Update() {}
 
+/**
+ * Render the component. Does nothing.
+ */
 void CounterComponent::Render() {}
 
+/**
+ * @brief Set a counter
+ *
+ * @param name Name of the counter to set
+ * @param value Value to set the counter to
+ */
 void CounterComponent::SetCounter(std::string name, int value) {
     m_counters[name] = value;
 }
 
+/**
+ * @brief Remove a counter
+ *
+ * @param name Name of the counter to remove
+ */
 void CounterComponent::RemoveCounter(std::string name) {
     m_counters.erase(name);
 }
 
+/**
+ * @brief Get a counter
+ *
+ * @param name Name of the counter to get
+ * @return int Value of the counter
+ */
 int CounterComponent::GetCounter(std::string name) { return m_counters[name]; }
