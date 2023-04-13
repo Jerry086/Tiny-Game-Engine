cbebdb28833c558f3e4dabf13e35d0a5119255c4
Jerry Wei <zywei86@gmail.com>
2023-04-12 21:06:44 -0700

    add comments to Vec2D

--- Engine/src/Vec.cpp
+++ Engine/src/Vec.cpp
@@ -1,11 +1,16 @@
 #include "Vec.hpp"
 
+/**
+ * Overload the + operator for Vec2
+ */
 Vec2 Vec2::operator+(Vec2 const &rhs)
 {
 
     return Vec2(x + rhs.x, y + rhs.y);
 }
-
+/**
+ * Overload the += operator for Vec2
+ */
 Vec2 &Vec2::operator+=(Vec2 const &rhs)
 {
     x += rhs.x;
@@ -13,7 +18,9 @@ Vec2 &Vec2::operator+=(Vec2 const &rhs)
 
     return *this;
 }
-
+/**
+ * Overload the * operator for Vec2
+ */
 Vec2 Vec2::operator*(float rhs)
 {
     return Vec2(x * rhs, y * rhs);
