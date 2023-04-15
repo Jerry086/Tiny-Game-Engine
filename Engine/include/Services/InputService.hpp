acf2fdc966d3095f6e88ade4a9b4c8cb5f3b3825
Szeka <tse.s@northeastern.edu>
2023-04-14 19:33:43 -0700

    input services draft

---
+++ Engine/include/Services/InputService.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "IService.hpp"
+
+class InputService : public IService {
+   public:
+    static InputService &instance();
+    void ShutDown() override;
+    void Update();
+
+    struct InputState {
+        bool quit = false;
+        bool up = false;
+        bool down = false;
+        bool left = false;
+        bool right = false;
+    };
+
+    InputState m_inputStateThisFrame;
+
+   private:
+    InputService() = default;
+    ~InputService() = default;
+    InputService(const InputService &) = delete;
+    InputService &operator=(const InputService &) = delete;
+};
---
+++ Engine/src/Services/InputService.cpp
@@ -0,0 +1,43 @@
+#include "./Services/InputService.hpp"
+
+#if defined(LINUX) || defined(MINGW)
+#include <SDL2/SDL.h>
+#else
+#include <SDL.h>
+#endif
+
+#include <cstdint>
+#include <iostream>
+
+InputService &InputService::instance() {
+    static InputService instance;
+    return instance;
+}
+
+void InputService::ShutDown() {}
+
+void InputService::Update() {
+    std::cout << "InputService::Update()" << std::endl;
+    // SDL_Event event;
+    // InputState state;
+    // while (SDL_PollEvent(&event)) {
+    //     if (event.type == SDL_QUIT) {
+    //         state.quit = true;
+    //     } else if (event.type == SDL_KEYDOWN) {
+    //         if (event.key.keysym.sym == SDLK_a ||
+    //             event.key.keysym.sym == SDLK_LEFT) {
+    //             state.left = true;
+    //         } else if (event.key.keysym.sym == SDLK_d ||
+    //                    event.key.keysym.sym == SDLK_RIGHT) {
+    //             state.right = true;
+    //         } else if (event.key.keysym.sym == SDLK_w ||
+    //                    event.key.keysym.sym == SDLK_UP) {
+    //             state.up = true;
+    //         } else if (event.key.keysym.sym == SDLK_s ||
+    //                    event.key.keysym.sym == SDLK_DOWN) {
+    //             state.down = true;
+    //         }
+    //     }
+    // }
+    // m_inputStateThisFrame = state;
+}
