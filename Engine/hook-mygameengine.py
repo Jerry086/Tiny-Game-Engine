9cccb0e3a9275ae6cf3dee4b5d7f1e4ad08324fa
Szeka <tse.s@northeastern.edu>
2023-04-18 12:46:23 -0700

    try not building scripts into binary?

--- Engine/hook-mygameengine.py
+++ Engine/hook-mygameengine.py
@@ -1,10 +1,10 @@
 from PyInstaller.utils.hooks import collect_data_files
 
 # # Collect files from the embedded scripts directory
-datas = collect_data_files('./Assets/Assets/scripts', include_py_files=True, subdir='./Assets/Assets/scripts')
+# datas = collect_data_files('./Assets/scripts', include_py_files=True, subdir='./Assets/scripts')
 
 # Collect files from the definitions directory
-datas += collect_data_files('./definitions', include_py_files=False, subdir='./definitions')
+datas = collect_data_files('./definitions', include_py_files=False, subdir='./definitions')
 
 # Collect files from the sprites directory
 datas += collect_data_files('./sprites', include_py_files=False, subdir='./sprites')
